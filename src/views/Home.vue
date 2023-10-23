830b714983414eecc586518bee2a025ddefe68a4
Gustavo Vasconcelos <gustavo.vasconcelos@thinslices.com>
2023-10-23 17:15:10 +0100

    Changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,58 +2,32 @@
 	<div>
 		<nav class="navbar is-fixed-top has-background-one" role="navigation">
 			<div class="navbar-brand">
-				<a
-					role="button"
-					class="navbar-burger burger"
-					:class="{ 'is-active': burgerActive }"
-					@click="burgerClicked"
-				>
+				<a role="button" class="navbar-burger burger" :class="{ 'is-active': burgerActive }" @click="burgerClicked">
 					<span aria-hidden="true"></span>
 					<span aria-hidden="true"></span>
 					<span aria-hidden="true"></span>
 				</a>
 			</div>
 
-			<div
-				class="navbar-menu"
-				:class="{
-					'is-active': burgerActive,
-					'has-background-one': !burgerActive,
-					'has-background-white': burgerActive,
-				}"
-				:style="
-					!burgerActive
-						? {
-								marginLeft: (windowWidth * 8) / 1920 + 'rem',
-								marginRight: (windowWidth * 8) / 1920 + 'rem',
-						}
-						: {}
-				"
-				@click="burgerClicked && windowWidth < 600"
-			>
+			<div class="navbar-menu" :class="{
+				'is-active': burgerActive,
+				'has-background-one': !burgerActive,
+				'has-background-white': burgerActive,
+			}" :style="!burgerActive
+	? {
+		marginLeft: (windowWidth * 8) / 1920 + 'rem',
+		marginRight: (windowWidth * 8) / 1920 + 'rem',
+	}
+	: {}
+	" @click="burgerClicked && windowWidth < 600">
 				<div class="navbar-start">
 					<div class="navbar-item">
-						<a
-							class="navbar-item"
-							:class="!burgerActive ? 'has-text-white' : ''"
-							v-scroll-to="'#about'"
-							@click="burgerActive = false"
-							>About me</a
-						>
-						<a
-							class="navbar-item"
-							:class="!burgerActive ? 'has-text-white' : ''"
-							v-scroll-to="'#skills'"
-							@click="burgerActive = false"
-							>Skills</a
-						>
-						<a
-							class="navbar-item"
-							:class="!burgerActive ? 'has-text-white' : ''"
-							v-scroll-to="'#projects'"
-							@click="burgerActive = false"
-							>Projects</a
-						>
+						<a class="navbar-item" :class="!burgerActive ? 'has-text-white' : ''" v-scroll-to="'#about'"
+							@click="burgerActive = false">About me</a>
+						<a class="navbar-item" :class="!burgerActive ? 'has-text-white' : ''" v-scroll-to="'#skills'"
+							@click="burgerActive = false">Skills</a>
+						<a class="navbar-item" :class="!burgerActive ? 'has-text-white' : ''" v-scroll-to="'#projects'"
+							@click="burgerActive = false">Projects</a>
 					</div>
 				</div>
 				<div class="navbar-end">
@@ -81,45 +55,27 @@
 				<hr />
 				<div class="columns">
 					<div class="column is-centered is-4" data-aos="fade-right">
-						<img class="is-rounded" src="../assets/img/me.jpg" />
+						<img class="is-rounded" src="../assets/img/me.jpeg" />
 						<div class="columns is-centered" style="margin-top: 5px">
-							<a
-								href="https://github.com/gustavo-vasconcelos/"
-								class="has-text-two"
-								target="_blank"
-							>
-								<span class="icon is-large" style="font-size: 22px"
-									><i class="fab fa-github" aria-hidden="true"></i
-								></span>
+							<a href="https://github.com/gustavo-vasconcelos/" class="has-text-two" target="_blank">
+								<span class="icon is-large" style="font-size: 22px"><i class="fab fa-github"
+										aria-hidden="true"></i></span>
 							</a>
-							<a
-								href="https://www.linkedin.com/in/gustavo-vasconcelos/"
-								class="has-text-two"
-								target="_blank"
-							>
-								<span class="icon is-large" style="font-size: 22px"
-									><i class="fab fa-linkedin" aria-hidden="true"></i
-								></span>
+							<a href="https://www.linkedin.com/in/gustavo-vasconcelos/" class="has-text-two" target="_blank">
+								<span class="icon is-large" style="font-size: 22px"><i class="fab fa-linkedin"
+										aria-hidden="true"></i></span>
 							</a>
 						</div>
 					</div>
-					<div
-						class="column is-centered has-text-justified has-text-white"
-						data-aos="fade-left"
-					>
+					<div class="column is-centered has-text-justified has-text-white" data-aos="fade-left">
 						<p>
-							I'm a senior undergrad student at the
-							<a
-								href="http://esmad.ipp.pt"
-								target="_blank"
-								class="has-text-four"
-								>ESMAD (P.PORTO)</a
-							>, studying Web Information Systems and Technologies. Currently
-							working at
-							<a class="has-text-four" href="https://www.celfocus.com/"
-								>Celfocus</a
-							>
-							as a full-stack developer in an IoT team.
+							I'm a developer with experience in web development, IoT solutions and API integration.
+							I also find the web development and IT field very fascinating and interesting, because we use
+							applications and services developed with these technologies in our daily lives.
+							During my academic studies at <a href="http://esmad.ipp.pt" target="_blank"
+								class="has-text-four">ESMAD (P.PORTO)</a>, I have developed many projects of which I'm very
+							proud of.
+							I'm proficient in both front and back end areas, but I like back-end the most.
 						</p>
 						<br />
 						<p>
@@ -143,47 +99,30 @@
 				<hr class="has-background-black" />
 				<div class="tabs is-toggle is-toggle-rounded is-fullwidth">
 					<ul>
-						<li
-							:class="selectedSkills.all ? 'is-active' : ''"
-							@click="changedSelectedSkills('all')"
-						>
+						<li :class="selectedSkills.all ? 'is-active' : ''" @click="changedSelectedSkills('all')">
 							<a>
-								<span class="icon is-small"
-									><i class="fas fa-th" aria-hidden="true"></i
-								></span>
+								<span class="icon is-small"><i class="fas fa-th" aria-hidden="true"></i></span>
 								<span>All</span>
 							</a>
 						</li>
-						<li
-							:class="selectedSkills.languages ? 'is-active' : ''"
-							@click="changedSelectedSkills('languages')"
-						>
+						<li :class="selectedSkills.languages ? 'is-active' : ''"
+							@click="changedSelectedSkills('languages')">
 							<a>
-								<span class="icon is-small"
-									><i class="fas fa-code" aria-hidden="true"></i
-								></span>
+								<span class="icon is-small"><i class="fas fa-code" aria-hidden="true"></i></span>
 								<span>Languages</span>
 							</a>
 						</li>
-						<li
-							:class="selectedSkills.technologies ? 'is-active' : ''"
-							@click="changedSelectedSkills('technologies')"
-						>
+						<li :class="selectedSkills.technologies ? 'is-active' : ''"
+							@click="changedSelectedSkills('technologies')">
 							<a>
-								<span class="icon is-small"
-									><i class="fas fa-cogs" aria-hidden="true"></i
-								></span>
+								<span class="icon is-small"><i class="fas fa-cogs" aria-hidden="true"></i></span>
 								<span>Technologies</span>
 							</a>
 						</li>
-						<li
-							:class="selectedSkills.softwares ? 'is-active' : ''"
-							@click="changedSelectedSkills('softwares')"
-						>
+						<li :class="selectedSkills.softwares ? 'is-active' : ''"
+							@click="changedSelectedSkills('softwares')">
 							<a>
-								<span class="icon is-small"
-									><i class="far fa-window-restore" aria-hidden="true"></i
-								></span>
+								<span class="icon is-small"><i class="far fa-window-restore" aria-hidden="true"></i></span>
 								<span>Softwares</span>
 							</a>
 						</li>
@@ -192,21 +131,12 @@
 				<br />
 				<div class="columns is-mobile is-multiline">
 					<template v-for="(skill, index) in skills">
-						<div
-							v-if="
-								selectedSkills.all ||
-								selectedSkills[skill.type] ||
-								selectedSkills[skill.type] === 'all'
-							"
-							class="column is-2-widescreen is-3-tablet is-4-mobile"
-							:key="'skill_' + index"
-							data-aos="fade-in"
-						>
+						<div v-if="selectedSkills.all ||
+							selectedSkills[skill.type] ||
+							selectedSkills[skill.type] === 'all'
+							" class="column is-2-widescreen is-3-tablet is-4-mobile" :key="'skill_' + index" data-aos="fade-in">
 							<a :href="skill.linkInfo" target="_blank">
-								<img
-									:title="skill.title"
-									:src="require(`@/assets/img/skills/${skill.img}`)"
-								/>
+								<img :title="skill.title" :src="require(`@/assets/img/skills/${skill.img}`)" />
 							</a>
 						</div>
 					</template>
@@ -218,54 +148,34 @@
 				<h1 class="title has-text-white">Projects</h1>
 				<hr />
 				<div class="columns is-multiline">
-					<div
-						class="column is-4-widescreen is-6-tablet"
-						v-for="(project, index) in projects"
-						:key="'project_' + index"
-						data-aos="fade-in"
-					>
+					<div class="column is-4-widescreen is-6-tablet" v-for="(project, index) in projects"
+						:key="'project_' + index" data-aos="fade-in">
 						<div class="card">
 							<div class="card-content" @click="openModal(project)">
 								<p class="is-size-4">{{ project.title }}</p>
 							</div>
 							<div class="card-image" @click="openModal(project)">
 								<figure class="image">
-									<img
-										:src="
-											require(`@/assets/img/projects/${project.title
-												.toLowerCase()
-												.replace(/ /g, '')}/thumb.jpg`)
-										"
-										alt
-									/>
+									<img :src="require(`@/assets/img/projects/${project.title
+										.toLowerCase()
+										.replace(/ /g, '')}/thumb.jpg`)
+										" alt />
 								</figure>
 							</div>
 
 							<footer class="card-footer">
 								<p class="card-footer-item" v-if="project.url.repository">
 									<span>
-										<a
-											:href="project.url.repository"
-											target="_blank"
-											class="has-text-three"
-											>GitHub</a
-										>
+										<a :href="project.url.repository" target="_blank" class="has-text-three">GitHub</a>
 									</span>
 								</p>
 								<p class="card-footer-item" v-if="project.url.demo">
 									<span>
-										<a
-											:href="project.url.demo"
-											target="_blank"
-											class="has-text-three"
-											>Live demo</a
-										>
+										<a :href="project.url.demo" target="_blank" class="has-text-three">Live demo</a>
 									</span>
 								</p>
 								<p class="card-footer-item">
-									<span class="has-text-three" @click="openModal(project)"
-										>More info</span
-									>
+									<span class="has-text-three" @click="openModal(project)">More info</span>
 								</p>
 							</footer>
 						</div>
@@ -273,43 +183,22 @@
 				</div>
 			</div>
 		</section>
-		<div
-			class="modal"
-			:class="{ 'is-active': modal.active }"
-			v-if="modal.active"
-		>
-			<div
-				class="modal-background"
-				@click="closeModal"
-				data-aos="fade-in"
-			></div>
+		<div class="modal" :class="{ 'is-active': modal.active }" v-if="modal.active">
+			<div class="modal-background" @click="closeModal" data-aos="fade-in"></div>
 			<div class="modal-card" data-aos="fade-up">
 				<header class="modal-card-head">
 					<p class="modal-card-title">{{ modal.project.title }}</p>
-					<button
-						class="delete"
-						aria-label="close"
-						@click="closeModal"
-					></button>
+					<button class="delete" aria-label="close" @click="closeModal"></button>
 				</header>
 				<section class="modal-card-body has-text-left">
-					<img
-						v-if="modal.active && !modal.project.url.promotionalVideo"
-						:src="
-							require(`../assets/img/projects/${modal.project.title
-								.toLowerCase()
-								.replace(/ /g, '')}/thumb.jpg`)
-						"
-					/>
-					<iframe
-						v-else-if="modal.project.url.promotionalVideo"
-						width="100%"
-						height="350"
-						:src="`https://www.youtube.com/embed/${modal.project.url.promotionalVideo}`"
-						frameborder="0"
+					<img v-if="modal.active && !modal.project.url.promotionalVideo" :src="require(`../assets/img/projects/${modal.project.title
+						.toLowerCase()
+						.replace(/ /g, '')}/thumb.jpg`)
+						" />
+					<iframe v-else-if="modal.project.url.promotionalVideo" width="100%" height="350"
+						:src="`https://www.youtube.com/embed/${modal.project.url.promotionalVideo}`" frameborder="0"
 						allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-						allowfullscreen
-					></iframe>
+						allowfullscreen></iframe>
 					<div class="content">
 						<div>
 							<p class="is-size-5">Description</p>
@@ -318,10 +207,7 @@
 						<div>
 							<p class="is-size-5">Technologies</p>
 							<ul>
-								<li
-									v-for="(technology, index) in modal.project.technologies"
-									:key="'technology_' + index"
-								>
+								<li v-for="(technology, index) in modal.project.technologies" :key="'technology_' + index">
 									{{ technology }}
 								</li>
 							</ul>
@@ -333,7 +219,7 @@
 									<li v-if="feature[0].length === 1" :key="'feature_' + index">
 										{{ feature }}
 									</li>
-									<li :key="'feature_' + index" v-else>
+									<li :key="'feature_' + index + 2" v-else>
 										{{ feature[0] }}
 										<ul>
 											<template v-for="(subfeature, subIndex) in feature">
@@ -349,20 +235,13 @@
 						<div v-if="modal.project.screenshots > 0">
 							<p class="is-size-5">Screenshots:</p>
 							<carousel :per-page="1" :centerMode="true" style="margin: 0">
-								<slide
-									data-index="0"
-									data-name="MySlideName"
-									v-for="(screenshot, index) in modal.project.screenshots"
-									:key="'screnshot_' + index"
-									style="margin: 0"
-								>
-									<img
-										:src="
-											require(`../assets/img/projects/${modal.project.title
-												.toLowerCase()
-												.replace(/ /g, '')}/screenshots/${index}.jpg`)
-										"
-									/>
+								<slide data-index="0" data-name="MySlideName"
+									v-for="(screenshot, index) in modal.project.screenshots" :key="'screnshot_' + index"
+									style="margin: 0">
+									<img :src="require(`../assets/img/projects/${modal.project.title
+										.toLowerCase()
+										.replace(/ /g, '')}/screenshots/${index}.jpg`)
+										" />
 								</slide>
 							</carousel>
 						</div>
@@ -377,56 +256,32 @@
 				<hr class="has-background-black" />
 				<div class="columns is-centered has-text-left is-multiline">
 					<div class="column is-6">
-						<div
-							class="notification is-two"
-							v-if="form.notification"
-							@click="form.notification = false"
-							data-aos="fade"
-						>
+						<div class="notification is-two" v-if="form.notification" @click="form.notification = false"
+							data-aos="fade">
 							Your message was sent. I'll reply you ASAP.
 						</div>
 						<form @submit.prevent="sendEmail">
 							<div class="field">
 								<label class="label" for="email">Your email</label>
 								<div class="control">
-									<input
-										id="email"
-										class="input"
-										type="email"
-										v-model="form.data.email"
-										required
-									/>
+									<input id="email" class="input" type="email" v-model="form.data.email" required />
 								</div>
 							</div>
 							<div class="field">
 								<label class="label" for="subject">Subject</label>
 								<div class="control">
-									<input
-										id="subject"
-										class="input"
-										type="text"
-										v-model="form.data.subject"
-										required
-									/>
+									<input id="subject" class="input" type="text" v-model="form.data.subject" required />
 								</div>
 							</div>
 							<div class="field">
 								<label class="label" for="message">Message</label>
 								<div class="control">
-									<textarea
-										id="message"
-										class="textarea"
-										name="message"
-										v-model="form.data.message"
-										required
-									></textarea>
+									<textarea id="message" class="textarea" name="message" v-model="form.data.message"
+										required></textarea>
 								</div>
 							</div>
-							<button
-								type="submit"
-								class="button is-two is-fullwidth"
-								:class="{ 'is-loading': form.sending }"
-							>
+							<button type="submit" class="button is-two is-fullwidth"
+								:class="{ 'is-loading': form.sending }">
 								Send
 							</button>
 						</form>
@@ -435,11 +290,10 @@
 			</div>
 		</section>
 		<footer id="footer" class="has-background-two has-text-white is-centered">
-			<a href="https://bulma.io/" class="has-text-four" target="_blank"
-				><img width="120" src="../assets/img/madewithbulma.png"
-			/></a>
+			<a href="https://bulma.io/" class="has-text-four" target="_blank"><img width="120"
+					src="../assets/img/madewithbulma.png" /></a>
 			<p style="margin-top: 10px">
-				Copyright &copy; 2019-2020 Gustavo Vasconcelos
+				Copyright &copy; 2019-2023 Gustavo Vasconcelos
 			</p>
 		</footer>
 	</div>
@@ -674,8 +528,8 @@ export default {
 				{
 					title: "Security System",
 					url: {
-						demo: "https://www.youtube.com/watch?v=buSrHclaZWY",
-						promotionalVideo: "buSrHclaZWY",
+						demo: "https://www.youtube.com/watch?v=Q1tRANREx",
+						promotionalVideo: "Q1tRANREx",
 					},
 					description:
 						"This project was developed as final project to the Computational Systems course unit, in order to practise all knowledge learnt. It was managed to create a password protected 'vault' that was locked/unlocked using a servo motor.",
@@ -692,7 +546,6 @@ export default {
 					url: {
 						repository: "https://github.com/gustavo-vasconcelos/StreetTeca",
 						demo: "https://gustavo-vasconcelos.github.io/StreetTeca/",
-						promotionalVideo: "cb7Wai93B60",
 					},
 					description:
 						"This project was developed during the 2nd semester of the 1st grade. The main purpose is to manage street libraries. There is no back-end involved, all information is kept locally (HTML Web Storage - Local Storage).",
@@ -719,7 +572,6 @@ export default {
 					title: "ATLAS",
 					url: {
 						repository: "https://github.com/gustavo-vasconcelos/ATLAS",
-						demo: "http://atlas-vue.herokuapp.com/",
 						promotionalVideo: "pC-v98n7apU",
 					},
 					description:
@@ -910,14 +762,17 @@ section:not([is-one]) .container .title {
 	box-shadow: 10px 10px 14px -4px rgba(0, 0, 0, 0.45);
 	max-width: 200px;
 }
+
 .button-hover {
 	color: #406e8e;
 	transition: all 0.1s ease-out;
 }
+
 .button-hover:hover {
 	color: white;
 	transition: all 0.1s ease-in;
 }
+
 section hr {
 	width: 100px;
 	text-decoration: underline;
@@ -932,23 +787,29 @@ section hr {
 	width: 100px;
 	transition: all 0.1s ease-in-out;
 }
+
 #skills img:hover {
 	transform: scale(1.2);
 	transition: all 0.1s ease-in-out;
 }
+
 .card {
 	border-radius: 6px;
 }
+
 .card.column {
 	padding: 0;
 }
+
 .card-content {
 	cursor: pointer;
 	padding: 0.5rem 0 0.5rem 0;
 }
+
 #projects .card .card-content p {
 	font-family: "Solway", serif;
 }
+
 .card-image,
 .card-footer-item span {
 	cursor: pointer;
@@ -964,22 +825,26 @@ section hr {
 	border-bottom-right-radius: 6px;
 }
 
-.modal .content div + div {
+.modal .content div+div {
 	margin-top: 20px;
 }
+
 .VueCarousel-pagination,
 .VueCarousel-dot-container {
 	margin-top: 0 !important;
 }
+
 .modal .content div p,
 .modal .content div ul {
 	margin-top: 0;
 	margin-bottom: 0;
 }
+
 #footer {
 	padding: 1.5rem 0 1.5rem 0;
 	font-size: 11px;
 }
+
 .notification {
 	cursor: pointer;
 }
@@ -987,14 +852,17 @@ section hr {
 .slide-fade-enter-active {
 	transition: all 0.3s ease;
 }
+
 .slide-fade-leave-active {
 	transition: all 0.3s ease;
 }
+
 .slide-fade-enter,
 .slide-fade-leave-to {
 	transform: translateX(10px);
 	opacity: 0;
 }
+
 #contact button {
 	font-family: "Montserrat", sans-serif;
 }
